7115e97194d72132ac9798b146a41e6f8f00a132
Matheus Josef <mjmatheusmj97@gmail.com>
2021-02-01 12:30:22 -0300

    Caminho

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Link from '../src/components/Link';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
+import GitHubCorner from '../src/components/GithubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 
