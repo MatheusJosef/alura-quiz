4119ba54cba970721a8a9a6cf02166eb998b194a
Matheus Josef <mjmatheusmj97@gmail.com>
2021-02-01 12:25:34 -0300

    Aula 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,17 +26,35 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Alura Quiz</title>
+        <meta name="title" content="Alura Quiz" />
+        <meta name="description" content="Desafie seus conhecimentos com o Alura Quiz" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Alura Quiz" />
+        <meta property="og:description" content="Desafie seus conhecimentos com o Alura Quiz" />
+        <meta property="og:image" content="https://wallpaperaccess.com/full/494420.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Alura Quiz" />
+        <meta property="twitter:description" content="Desafie seus conhecimentos com o Alura Quiz" />
+        <meta property="twitter:image" content="https://wallpaperaccess.com/full/494420.jpg" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
